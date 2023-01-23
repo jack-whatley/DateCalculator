8383fec6f5f8ae3734f3bee994beb38839142d7e
jack-whatley <wickselm2005@gmail.com>
2023-01-23 14:55:48 +0000

    working on ytdl

--- DateCalculator/ViewModel/DownloadViewModel.cs
+++ DateCalculator/ViewModel/DownloadViewModel.cs
@@ -1,13 +1,42 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Text;
 
 namespace DateCalculator.ViewModel
 {
     class DownloadViewModel : BaseViewModel
     {
-        public DownloadViewModel() { }
+        public DownloadViewModel() 
+        {
+            ButtonPress = new RelayCommand(DownloadVideo);
+        }
 
+        public RelayCommand ButtonPress { get; set; }
 
+        public void DownloadVideo(object obj)
+        {
+            string YTDL = @"/C C:\Users\the-c\Desktop\youtube-downloader\youtube-dl.exe";
+            string args = "https://youtu.be/BoCx1MvfcUU";
+            string command = $"{YTDL} --help"; // {args}
+
+            ProcessStartInfo psi = new ProcessStartInfo("cmd", "/c" + command);
+
+            try
+            {
+                System.Diagnostics.Process.Start("cmd.exe", command);
+                /*using(Process process = new Process())
+                {
+                    process.StartInfo = psi;
+                    process.Start();
+
+                    process.WaitForExit();
+                }*/
+            }
+            catch
+            {
+                Debug.WriteLine("ytdl failed");
+            }
+        }
     }
 }
